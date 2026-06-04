4296d801b960a0a7e670b786a5128b95dc1d646d
zengwangcheng <38219893+greenhand0403@users.noreply.github.com>
2026-06-04 15:11:28 +0800

    代码结构整理

--- GPOG2000A_Demo/main.c
+++ GPOG2000A_Demo/main.c
@@ -86,7 +86,6 @@ int VC_Mode;
 int ShiftPitchIdx;
 int ConstPitchIdx;
 int EchoGainIdx;
-unsigned int VcVolIdx;
 int ADC_FIR_Type;
 int DAC_FIR_Type;
 unsigned A1800_Idx = 0;
@@ -124,6 +123,7 @@ void Auto_StartPlayRecorded(void);
 void Auto_StopWorkMode(void);
 void CMPADC_Stop(void);
 unsigned char KeyCount = 0;// 按第1次先进入工作模式，默认是高音调模式；按第2次，还是在工作模式，但是切换低音调；按第3次，还是在工作模式，但是机器人音效；按第4次还是播放一次滴声，然后退出工作模式
+#define MAX_SOUND_EFFECT 3// 3种有效的变声模式高音调 低音调 机器人音调
 #define DI_SOUND_TIMEOUT_COUNT   60000UL
 volatile unsigned char KeyBusy = 0;//上拉按键忙
 volatile unsigned char AutoBusy = 0;//处于前面三种变声自动监听状态，需要处理状态机
@@ -145,9 +145,14 @@ extern unsigned R_ADCKeyRaw;
 unsigned Last_IOA7_ADC_Key = ADC_KEY_NONE;
 // 正在做的自动录音10秒
 void Do_IOA7_LowPress_RecorderAction(void);
+// 为ADC按键 下拉按键自动录音并播放设计的四种状态，对应 未按下时空闲、按下后自动录音、录音完自动播放、退出ADC按键模式变成数字按键上拉按键
+#define KEYDOWN_LOW_IDLE              0
+#define KEYDOWN_LOW_RECORDING     1
+#define KEYDOWN_LOW_PLAYING       2
+#define KEYDOWN_HIGH_MODE    3
 // 用于调试
 unsigned keydown_rec = 0;
-
+#define KEYDOWN_REC_TIME (64*5)
 volatile unsigned g_tmaDiv = 0;
 volatile unsigned g_tma64Ticks = 0;
 
@@ -165,7 +170,6 @@ int main()
 	ShiftPitchIdx = 0;
 	ConstPitchIdx = 0;    
   	EchoGainIdx = 4;
-	VcVolIdx = 12;
 	// 音量检测
 	AutoState = AUTO_IDLE;
 	LastAttackCount = 0;
@@ -176,7 +180,7 @@ int main()
 	// keydown_rec = 10;
 	while(1)
 	{
-		if (keydown_rec == 10)
+		if (keydown_rec == KEYDOWN_HIGH_MODE)
 		{
 			Handle_Key();
 		}
@@ -190,11 +194,11 @@ int main()
 		SACM_DVR1800_ServiceLoop();
 		
 		// 下拉按键模式的简单录音并自动播放的状态机
-		if(keydown_rec == 1)
+		if(keydown_rec == KEYDOWN_LOW_RECORDING)
 		{
-			if (g_tma64Ticks >= 64*5)
+			if (g_tma64Ticks >= KEYDOWN_REC_TIME)
 			{
-				keydown_rec = 2;
+				keydown_rec = KEYDOWN_LOW_PLAYING;
 
 				WatchdogClear();
 				SACM_DVR1800_Stop();
@@ -221,18 +225,19 @@ int main()
 				SACM_A1800_fptr_Play(Manual_Mode_Index, DAC1, 0);
 				
 				SACM_VC4_Initial();			// VC4 initial
+				SACM_VC4_Volume(65535);// 最大声
 		        SACM_VC4_Mode(VC_Mode, &VC4WorkRam);         
 				SACM_VC4_Play(Manual_Mode_Index, DAC1, Ramp_Up + Ramp_Dn);	// manual mode playback
 			}
-		}else if (keydown_rec == 2)
+		}else if (keydown_rec == KEYDOWN_LOW_PLAYING)
 		{
 			// 播放录音完毕，重新允许IOA7 ADC按键
 			if ((SACM_VC4_Status() & 0x01) == 0)
 			{
-				keydown_rec = 0;
+				keydown_rec = KEYDOWN_LOW_IDLE;
 				CMPADC_IOA7Key_Init();
 			}
-		}else if (keydown_rec == 10)
+		}else if (keydown_rec == KEYDOWN_HIGH_MODE)
 		{
 			// 上拉按键模式的音量检测状态机
 			if (AutoState != AUTO_IDLE)
@@ -253,171 +258,31 @@ int main()
 void Handle_Key(void)
 {
 	Key = SP_GetCh();
-	switch(Key)
-	{	
-		case 0x0001:	// IOA0 + Vcc
-			if(chk_MIC_voice_flag == 1)
-				chk_MIC_voice_flag = 0;	   ////stop MIC EnvDet
-			SACM_A1800_fptr_Stop();
-			SACM_VC4_Stop();
-			// 擦除录音数据
-			__asm("INT OFF");
-			MoveSPIDriverToRAM_0();
-			MoveSPIDriverToRAM_2();
-			SPI_Flash_Block_Erase(R_REC_block);
-			SPI_Flash_Block_Erase(R_REC_block + 1);
-			CMPADC_Init();
-			*P_INT_Ctrl = C_IRQ0_TMA | C_IRQ3_ADC;			//Allow TMA, ADC interrupt only.
-			__asm("INT FIQ,IRQ");
-			
-			MoveSPIDriverToRAM_0();
-			MoveSPIDriverToRAM_1();
-			SACM_DVR1800_Initial();
-			USER_DVR1800_SetStartAddr(0x4, R_REC_block);			// skip 4 Bytes for length header
-			SACM_DVR1800_Rec(RecMonitorOff, Mic, DVR1800_BIT_RATE_16K);
-			break;
-		
-		case 0x0002:	// IOA1 + Vcc
-			SACM_DVR1800_Stop();
-			break;
-
-		case 0x0004:	// IOA2 + Vcc
-			if(chk_MIC_voice_flag == 1)
-				chk_MIC_voice_flag = 0;	   ////stop MIC EnvDet
-			SACM_A1800_fptr_Initial();                 // A1800 initial
-			USER_A1800_fptr_Volume(9);
-			A1800_fptr_Event_Initial();	
-			A1800_fptr_IO_Event_Enable();
-			// VolCompressInitial();
-			// SetVolCompressLevel(12);
-			SACM_A1800_fptr_Stop();
-
-			Block_Addr = (R_REC_block * 65536)/2;
-			Block_Addr = Block_Addr + 0x8000;
-			DVR18_ExtMem_Low = Block_Addr & 0xffff;
-			DVR18_ExtMem_High = Block_Addr >> 16;
-
-			SACM_A1800_fptr_Play(Manual_Mode_Index, DAC1, 0);
-			
-			SACM_VC4_Initial();			// VC4 initial
-			SACM_VC4_AD_FIRType(ADC_FIR_Type);
-			SACM_VC4_DA_FIRType(DAC_FIR_Type);
-			SACM_VC4_Volume(65535);// 最大声
-
-			// SACM_VC4_Volume_Control(C_Volume_Control_Enable);
-
-			switch(EffectMode)
-			{
-				case 0:     // 高音调
-					VC_Mode = VC4_SHIFT_PITCH_MODE;
-					SACM_VC4_Mode(VC_Mode, &VC4WorkRam);
-			
-					ShiftPitchIdx = 8;
-					SACM_VC4_ShiftPitch(ShiftPitchIdx, &VC4WorkRam);
-					break;
-			
-				case 1:     // 低音调
-					VC_Mode = VC4_SHIFT_PITCH_MODE;
-					SACM_VC4_Mode(VC_Mode, &VC4WorkRam);
-			
-					ShiftPitchIdx = -4;
-					SACM_VC4_ShiftPitch(ShiftPitchIdx, &VC4WorkRam);
-					break;
-			
-				case 2:     // 机器人音调
-					VC_Mode = VC4_RobotEffect1;
-					SACM_VC4_Mode(VC_Mode, &VC4WorkRam);
-					break;
-			
-				default:
-					EffectMode = 0;
-					break;
-			}
-			
-			EffectMode++;
-			if(EffectMode >= 3)
-			{
-				EffectMode = 0;
-			}
-
-			SACM_VC4_Mode(VC4_SHIFT_PITCH_MODE, &VC4WorkRam); 
-			// SACM_VC4_ShiftPitch(0, &VC4WorkRam); 
-			SACM_VC4_Play(Manual_Mode_Index, DAC1, Ramp_Up + Ramp_Dn);	// manual mode playback
-			break;
-
-		case 0x0080:    // IOA7 + Vcc
-			// 自动监听式变声器模式
-			if (KeyBusy)
-				break;
-			KeyBusy = 1;
+	if(Key == 0x0080)
+	{
+		// 自动监听式变声器模式
+		if (KeyBusy)
+			return;
+		KeyBusy = 1;
+		PlayDiSound();
+		EffectMode = KeyCount;
+	
+		if (KeyCount < MAX_SOUND_EFFECT)
+		{
+			KeyCount++;
+			AutoState = AUTO_IDLE;
+			Auto_PrepareRecord();
+			AutoState = AUTO_WAIT_ATTACK;
+		}
+		else
+		{
 			PlayDiSound();
-			EffectMode = KeyCount;
-		
-			if (KeyCount < 3)
-			{
-				KeyCount++;
-				AutoState = AUTO_IDLE;
-				Auto_PrepareRecord();
-				AutoState = AUTO_WAIT_ATTACK;
-			}
-			else
-			{
-				PlayDiSound();
-				KeyCount = 0;
-				Auto_StopWorkMode();
-			}
-		
-			KeyBusy = 0;
-			break;
-
-		case 0x0010:	// IOA4 + Vcc	
-			// 停止播放	
-			SACM_A1800_fptr_Stop();
-			SACM_VC4_Stop();
-			break;
-			
-		case 0x0020:	// IOA5 + Vcc
-			chk_MIC_voice_flag = 1;   ////start MIC EnvDet
-			CMPADC_Init();
-			EnvDet_Initial();										//Envelope initial
-			EnvDet_SetAttackLevel(EnvDet_AttackLevel);  //音量增大门槛值
-			EnvDet_SetAttackTime(EnvDet_AttackTime);   //音量增大到门槛值后持续时间
-			EnvDet_SetReleaseLevel(EnvDet_ReleaseLevel); //音量减小门槛值
-			EnvDet_SetReleaseTime(EnvDet_ReleaseTime);  //音量减小到门槛值后持续时间
-			EnvDet_Start();
-			
-			break;
-			
-		case 0x0040:	// IOA6 + Vcc		
-			EnvDet_Stop();
-			chk_MIC_voice_flag = 0;	   ////stop MIC EnvDet
-			__asm("clrb [0x3005], 1");  //P_IOB_Buffer			0x3005	
-			__asm("clrb [0x3001], 7");  //P_IOA_Buffer
-			break;
-			
-		case 0x0800:	// IOA11 + Vcc		
-			if(chk_MIC_voice_flag == 1)
-				chk_MIC_voice_flag = 0;	   ////stop MIC EnvDet
-			*P_INT_Ctrl &= ~C_IRQ3_ADC;			// ADC interrupt off,when VC4 Play;
-			SACM_A1800_fptr_Initial();                 // A1800 initial
-			USER_A1800_fptr_Volume(9);
-			A1800_fptr_Event_Initial();	
-			A1800_fptr_IO_Event_Enable();
-			SACM_A1800_fptr_Stop();
-			A1800_Idx = 0;
-			USER_A1800_fptr_SetStartAddr(A1800_Idx);    // Set index address
-			SACM_A1800_fptr_Play(Manual_Mode_Index, DAC1, 0);
-			
-			SACM_VC4_Initial();			// VC4 initial
-			// SACM_VC4_Volume_Control(C_Volume_Control_Enable);       	
-			// VC_Mode = VC4_SHIFT_PITCH_MODE;
-			SACM_VC4_Mode(VC_Mode, &VC4WorkRam);	                    
-			SACM_VC4_Play(Manual_Mode_Index, DAC1, Ramp_Up + Ramp_Dn);	// manual mode playback
-			break;
-			
-		default:
-			break;
-	} // end of switch
+			KeyCount = 0;
+			Auto_StopWorkMode();
+		}
+	
+		KeyBusy = 0;
+	}
 }
 void Auto_StateMachine(void)
 {
@@ -697,7 +562,7 @@ void Handle_IOA7_ADC_Key(void)
 {
     unsigned adcKey;
 
-	if (keydown_rec != 0)
+	if (keydown_rec != KEYDOWN_LOW_IDLE)
 		return;
 
     // 只在空闲态检测，避免和 EnvDet / 自动录音 / 自动播放抢 CMPADC
@@ -720,7 +585,7 @@ void Handle_IOA7_ADC_Key(void)
 		{
 			// 永久退出 IOA7 ADC 按键模式，将 CMPADC 交给麦克风静音检测使用
 
-			keydown_rec = 10;
+			keydown_rec = KEYDOWN_HIGH_MODE;
 
 			KeyBusy = 1;
 
@@ -728,7 +593,7 @@ void Handle_IOA7_ADC_Key(void)
 
 			EffectMode = KeyCount;
 
-			if (KeyCount < 3)
+			if (KeyCount < MAX_SOUND_EFFECT)
 			{
 				KeyCount++;
 
@@ -751,28 +616,6 @@ void Handle_IOA7_ADC_Key(void)
 }
 void Do_IOA7_LowPress_RecorderAction(void)
 {
-	// 播放滴声
-    // *P_INT_Ctrl &= ~C_IRQ3_ADC;			// ADC interrupt off,when VC4 Play;
-	// SACM_A1800_fptr_Initial();                 // A1800 initial
-	// A1800_fptr_Event_Initial();	
-	// A1800_fptr_IO_Event_Enable();
-	// SACM_A1800_fptr_Stop();
-	// USER_A1800_fptr_SetStartAddr(0);    // Set index address
-	// SACM_A1800_fptr_Play(Manual_Mode_Index, DAC1, 0);
-	
-	// SACM_VC4_Initial();			// VC4 initial
-	// SACM_VC4_Mode(VC_Mode, &VC4WorkRam);	                    
-	// SACM_VC4_Play(Manual_Mode_Index, DAC1, Ramp_Up + Ramp_Dn);
-	// // 等待播放滴声完毕
-	// SACM_VC4_ServiceLoop();
-	// SACM_DVR1800_ServiceLoop();
-	// System_ServiceLoop();
-	// while ((SACM_VC4_Status() & 0x01) != 0)
-	// {
-	// 	SACM_VC4_ServiceLoop();
-	// 	SACM_DVR1800_ServiceLoop();
-	// 	System_ServiceLoop();
-	// }
 	// 擦除录音数据
 	SACM_DVR1800_Stop();
 	
@@ -797,7 +640,7 @@ void Do_IOA7_LowPress_RecorderAction(void)
     g_tma64Ticks = 0;
 	
 	SACM_DVR1800_Rec(RecMonitorOff, Mic, DVR1800_BIT_RATE_16K);
-	keydown_rec = 1;
+	keydown_rec = KEYDOWN_LOW_RECORDING;
 }
 void Wait_TMA64_Ticks(unsigned ticks)
 {
